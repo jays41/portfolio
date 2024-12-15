e0ceaf5bd3f9341bddcd09a62f6703c9ba7ff416
jayshah41 <jay.1.shah@kcl.ac.uk>
2024-12-15 23:13:27 +0000

    Add show/hide functionality for education section

--- portfolio/src/Education.jsx
+++ portfolio/src/Education.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Education = () => {
+
+  const [isShowing, show] = useState(false);
+
   return (
     <section className="section">
         <h2>EDUCATION</h2>
-            <p><strong>King’s College London</strong> - Expected Graduation May 2027</p>
-            <p>BSc Computer Science (expected grade: 1st Class Honours)</p>
-            <p><strong>Roundwood Park School</strong> - 2016-2023</p>
-            <p>A-Levels: Maths (A*), Further Maths (A*), Computer Science (A), Physics (A)</p>
-            <p>GCSEs: 999999999</p>
+        {isShowing ? <button onClick={() => {show(false)}}>Show</button> : 
+        <>
+          <p><strong>King’s College London</strong> - Expected Graduation May 2027</p>
+          <p>BSc Computer Science (expected grade: 1st Class Honours)</p>
+          <p><strong>Roundwood Park School</strong> - 2016-2023</p>
+          <p>A-Levels: Maths (A*), Further Maths (A*), Computer Science (A), Physics (A)</p>
+          <p>GCSEs: 999999999</p>
+          <button onClick={() => {show(true)}}>Hide</button>
+        </>
+        }
     </section>
   )
 }
