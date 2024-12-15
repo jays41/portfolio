83b1f8887e8198d34abf00697b8e65d6c48d52b2
jayshah41 <jay.1.shah@kcl.ac.uk>
2024-12-15 23:08:16 +0000

    show/hide content buttons for projects section

--- portfolio/src/Projects.jsx
+++ portfolio/src/Projects.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Projects = () => {
+
+    const [isShowing, show] = useState(false);
+
 return (
     <section className="section">
-            <h2>PERSONAL PROJECTS</h2>
+        <h2>PERSONAL PROJECTS</h2>
+        {isShowing ? <button onClick={() => {show(false)}}>Show</button> : 
+        <>
             <p><strong>King’s Capital (King’s Business Club)</strong> - London, Quantitative Trading Associate, October 2024 - Present</p>
             <p>Worked with the Quant department to develop algorithms and execute trades.</p>
             <p>Drafted papers on quantitative strategies, including LSTMs in prediction strategies.</p>
@@ -11,6 +17,9 @@ return (
             <p>Developed an LSTM model in TensorFlow to forecast stock prices.</p>
             <p>Built a sentiment analysis tool for evaluating investor sentiment based on news and social media.</p>
             <p>Designed a UI with JavaScript and a backend in Python using Flask.</p>
+            <button onClick={() => {show(true)}}>Hide</button>
+        </>
+        }
     </section>
 )
 }
