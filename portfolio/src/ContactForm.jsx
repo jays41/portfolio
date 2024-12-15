61b1410dfa61cce662c03fb15488f5be587d32c9
jayshah41 <jay.1.shah@kcl.ac.uk>
2024-12-15 22:42:05 +0000

    styling

--- portfolio/src/ContactForm.jsx
+++ portfolio/src/ContactForm.jsx
@@ -2,25 +2,26 @@ import React from 'react'
 
 const ContactForm = () => {
   return (
-    <div>
+    <section className="section">
+        <h2>Contact Me</h2>
         <form>
             <div>
                 <p>
-                    <label htmlFor="name">Name:</label>
+                    <label htmlFor="name">Name: </label>
                     <input type="text" id="name" name="name" required />
                 </p>
                 <p>
-                    <label htmlFor="email">Email:</label>
+                    <label htmlFor="email">Email: </label>
                     <input type="email" id="email" name="email" required />
                 </p>
                 <p>
-                    <label htmlFor="message">Message:</label>
+                    <label htmlFor="message">Message: </label>
                     <textarea id="message" name="message" required></textarea>
                 </p>
                 <button type="submit">Submit</button>
             </div>
         </form>
-    </div>
+    </section>
   )
 }
 
