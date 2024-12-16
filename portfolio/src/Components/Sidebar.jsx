66ad7d1bb16337118bc0fef210a6ad2eab287368
jayshah41 <jay.1.shah@kcl.ac.uk>
2024-12-16 21:35:37 +0000

    Refactor Sidebar component to use a div element for improved structure

--- portfolio/src/Components/Sidebar.jsx
+++ portfolio/src/Components/Sidebar.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import './Sidebar.css';
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import { HashLink } from 'react-router-hash-link';
 
 const Sidebar = () => {
   return (
-    <nav className='sidebar'>
-        <a href="#About">About</a>
-        <a href="#Skills">Skills</a>
-        <a href="#Education">Education</a>
-        <a href="#Experience">Experience</a>
-        <a href="#Projects">Projects</a>
-        <a href="#CV">CV</a>
-        <a href="#ContactForm">Contact</a>
-        <a href="#Links">Links</a>
-    </nav>
+    <div className='sidebar'>
+      <a href="#About">About</a>
+      <a href="#Skills">Skills</a>
+      <a href="#Education">Education</a>
+      <a href="#Experience">Experience</a>
+      <a href="#Projects">Projects</a>
+      <a href="#CV">CV</a>
+      <a href="#ContactForm">Contact</a>
+      <a href="#Links">Links</a>
+    </div>
   )
 }
 
