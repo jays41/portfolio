4b463b5e591020f6590bf7fbe2323ab5ddfc29ac
jayshah41 <jay.1.shah@kcl.ac.uk>
2024-12-16 16:14:54 +0000

    Add modal functionality to ProjectTile component for displaying project details

--- portfolio/src/Components/ProjectTile.jsx
+++ portfolio/src/Components/ProjectTile.jsx
@@ -1,26 +1,36 @@
-import React from 'react'
-import { useState } from 'react'
-import './ProjectTile.css'
-import TagContainer from './TagContainer'
+import React, { useState } from 'react';
+import './ProjectTile.css';
+import TagContainer from './TagContainer';
 
 const ProjectTile = ({ heading, description, tags }) => {
+    const [expanded, setExpanded] = useState(false);
 
-    const [expanded, expand] = useState(false);
+    const closeModal = () => setExpanded(false);
 
     return (
-    <div className="tile">
-        <h3 className="tile-heading">{heading}</h3>
-        { expanded ?
-        <div>
-            <p className="tile-description">{description}</p>
-            <button onClick={() => (expand(false))}>Hide Details</button>
-        </div>
-        :
-        <button onClick={() => (expand(true))}>Show Details</button>
-        }
-        <TagContainer tags={tags} />
-    </div>
-    )
-}
+        <>
+            <div className="tile">
+                <h3 className="tile-heading">{heading}</h3>
+                <button onClick={() => setExpanded(true)}>Show Details</button>
+                <TagContainer tags={tags} />
+            </div>
 
-export default ProjectTile
+            {/* Render the modal at the top level */}
+            {expanded && (
+                <div className="modal-overlay" onClick={closeModal}>
+                    <div
+                        className="modal-content"
+                        onClick={(e) => e.stopPropagation()} // Prevent closing modal when clicking inside
+                    >
+                        <h3>{heading}</h3>
+                        <p>{description}</p>
+                        <TagContainer tags={tags} />
+                        <button onClick={closeModal}>Close</button>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default ProjectTile;
