d86e54d8d6a6aab2971c06ea011b5a3260384bcb
jayshah41 <jay.1.shah@kcl.ac.uk>
2024-12-16 15:33:21 +0000

    Add expand/collapse functionality to ProjectTile component for description

--- portfolio/src/Components/ProjectTile.jsx
+++ portfolio/src/Components/ProjectTile.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
 import './ProjectTile.css'
 import TagContainer from './TagContainer'
 
 const ProjectTile = ({ heading, description, tags }) => {
+
+    const [expanded, expand] = useState(false);
+
     return (
     <div className="tile">
         <h3 className="tile-heading">{heading}</h3>
-        <p className="tile-description">{description}</p>
+        { expanded ?
+        <div>
+            <p className="tile-description">{description}</p>
+            <button onClick={() => (expand(false))}>Hide Details</button>
+        </div>
+        :
+        <button onClick={() => (expand(true))}>Show Details</button>
+        }
         <TagContainer tags={tags} />
     </div>
     )
