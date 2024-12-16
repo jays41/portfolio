0b48687ffed6957ab934c6c35b1e8cf807f2f22a
jayshah41 <jay.1.shah@kcl.ac.uk>
2024-12-16 02:34:29 +0000

    Add LinkedIn and GitHub icons with links to the footer

--- portfolio/src/Components/Links.jsx
+++ portfolio/src/Components/Links.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import linkedin from '../assets/images/links/linkedin.png'
+import github from '../assets/images/links/github.png'
+import info from '../Info.json'
 
 const Links = () => {
+
   return (
     <footer>
         <h4>My Links:</h4>
-        <a href="https://www.linkedin.com/in/jayrshah41/">LinkedIn</a>
+        <br />
+        <div className="images">
+          <a href={info.links.linkedin_url} target="_blank" rel="noopener noreferrer">
+              <img src={linkedin} alt="LinkedIn" />
+          </a>
+          <a href={info.links.github_url} target="_blank" rel="noopener noreferrer">
+              <img src={github} alt="GitHub" />
+          </a>
+        </div>
     </footer>
   )
 }
