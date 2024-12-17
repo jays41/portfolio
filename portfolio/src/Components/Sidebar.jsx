f5c02e1401ff2ae8463b691a805058de4b27fc07
jayshah41 <jay.1.shah@kcl.ac.uk>
2024-12-17 00:39:30 +0000

    Replace anchor tags with AnchorLink components in Sidebar for smooth scrolling functionality

--- portfolio/src/Components/Sidebar.jsx
+++ portfolio/src/Components/Sidebar.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
+import React from 'react';
 import './Sidebar.css';
-import AnchorLink from 'react-anchor-link-smooth-scroll'
-import { HashLink } from 'react-router-hash-link';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      <a href="#About">About</a>
-      <a href="#Skills">Skills</a>
-      <a href="#Education">Education</a>
-      <a href="#Experience">Experience</a>
-      <a href="#Projects">Projects</a>
-      <a href="#CV">CV</a>
-      <a href="#ContactForm">Contact</a>
-      <a href="#Links">Links</a>
+      <AnchorLink smooth href="#About">About</AnchorLink>
+      <AnchorLink smooth href="#Skills">Skills</AnchorLink>
+      <AnchorLink smooth href="#Education">Education</AnchorLink>
+      <AnchorLink smooth href="#Experience">Experience</AnchorLink>
+      <AnchorLink smooth href="#Projects">Projects</AnchorLink>
+      <AnchorLink smooth href="#CV">CV</AnchorLink>
+      <AnchorLink smooth href="#ContactForm">Contact</AnchorLink>
+      <AnchorLink smooth href="#Links">Links</AnchorLink>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
