8fcd8c3e076079641331499b47b19c9c601556f0
jayshah41 <jay.1.shah@kcl.ac.uk>
2025-01-13 19:13:54 +0000

    Update Sidebar component to enable smooth scrolling for all links

--- portfolio/src/Components/Sidebar.jsx
+++ portfolio/src/Components/Sidebar.jsx
@@ -5,14 +5,14 @@ import AnchorLink from 'react-anchor-link-smooth-scroll';
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      <AnchorLink smooth href="#About">About</AnchorLink>
-      <AnchorLink smooth href="#Skills">Skills</AnchorLink>
-      <AnchorLink smooth href="#Education">Education</AnchorLink>
-      <AnchorLink smooth href="#Experience">Experience</AnchorLink>
-      <AnchorLink smooth href="#Projects">Projects</AnchorLink>
-      <AnchorLink smooth href="#CV">CV</AnchorLink>
-      <AnchorLink smooth href="#ContactForm">Contact</AnchorLink>
-      <AnchorLink smooth href="#Links">Links</AnchorLink>
+      <AnchorLink smooth="true" href="#About">About</AnchorLink>
+      <AnchorLink smooth="true" href="#Skills">Skills</AnchorLink>
+      <AnchorLink smooth="true" href="#Experience">Experience</AnchorLink>
+      <AnchorLink smooth="true" href="#Education">Education</AnchorLink>
+      <AnchorLink smooth="true" href="#Projects">Projects</AnchorLink>
+      <AnchorLink smooth="true" href="#CV">CV</AnchorLink>
+      <AnchorLink smooth="true" href="#ContactForm">Contact</AnchorLink>
+      <AnchorLink smooth="true" href="#Links">Links</AnchorLink>
     </div>
   );
 }
