264bbd57a0cb88e323d0032baf9cac0e312ae74f
jayshah41 <jay.1.shah@kcl.ac.uk>
2025-01-14 23:57:54 +0000

    Add toggle functionality to Sidebar for showing and hiding links

--- portfolio/src/Components/Sidebar.jsx
+++ portfolio/src/Components/Sidebar.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
+import { useState } from 'react';
 import './Sidebar.css';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Sidebar = () => {
+
+  const [isShowing, show] = useState(true);
+
   return (
-    <div className='sidebar'>
-      <AnchorLink smooth="true" href="#About">About</AnchorLink>
-      <AnchorLink smooth="true" href="#Skills">Skills</AnchorLink>
-      <AnchorLink smooth="true" href="#Experience">Experience</AnchorLink>
-      <AnchorLink smooth="true" href="#Education">Education</AnchorLink>
-      <AnchorLink smooth="true" href="#Projects">Projects</AnchorLink>
-      <AnchorLink smooth="true" href="#CV">CV</AnchorLink>
-      <AnchorLink smooth="true" href="#ContactForm">Contact</AnchorLink>
-      <AnchorLink smooth="true" href="#Links">Links</AnchorLink>
-    </div>
+    <>
+    {!isShowing ? <></> :
+      <div className='sidebar'>
+        <button onClick={()=>{show(false)}}>Hide Sidebar</button>
+        <AnchorLink smooth="true" href="#About">About</AnchorLink>
+        <AnchorLink smooth="true" href="#Skills">Skills</AnchorLink>
+        <AnchorLink smooth="true" href="#Experience">Experience</AnchorLink>
+        <AnchorLink smooth="true" href="#Education">Education</AnchorLink>
+        <AnchorLink smooth="true" href="#Projects">Projects</AnchorLink>
+        <AnchorLink smooth="true" href="#CV">CV</AnchorLink>
+        <AnchorLink smooth="true" href="#ContactForm">Contact</AnchorLink>
+        <AnchorLink smooth="true" href="#Links">Links</AnchorLink>
+      </div>
+    }
+    </>
   );
 }
 
