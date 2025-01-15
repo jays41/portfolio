cf84ecda6d4316ffde7826522573534f5693c3bf
jayshah41 <jay.1.shah@kcl.ac.uk>
2025-01-15 21:53:55 +0000

    Add Loader component with CSS for animated loading effect

---
+++ portfolio/src/Components/Loader.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import './Loader.css'
+
+const Loader = () => {
+  return (
+    //  From Uiverse.io by Praashoo7  
+    <div class="main">
+    <div class="up">
+        <div class="loaders">
+        <div class="loader"></div>
+        <div class="loader"></div>
+        <div class="loader"></div>
+        <div class="loader"></div>
+        <div class="loader"></div>
+        <div class="loader"></div>
+        <div class="loader"></div>
+        <div class="loader"></div>
+        <div class="loader"></div>
+        <div class="loader"></div>
+        </div>
+        <div class="loadersB">
+        <div class="loaderA">
+            <div class="ball0"></div>
+        </div>
+        <div class="loaderA">
+            <div class="ball1"></div>
+        </div>
+        <div class="loaderA">
+            <div class="ball2"></div>
+        </div>
+        <div class="loaderA">
+            <div class="ball3"></div>
+        </div>
+        <div class="loaderA">
+            <div class="ball4"></div>
+        </div>
+        <div class="loaderA">
+            <div class="ball5"></div>
+        </div>
+        <div class="loaderA">
+            <div class="ball6"></div>
+        </div>
+        <div class="loaderA">
+            <div class="ball7"></div>
+        </div>
+        <div class="loaderA">
+            <div class="ball8"></div>
+        </div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Loader
