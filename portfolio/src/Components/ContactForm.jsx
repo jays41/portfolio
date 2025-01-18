661c18a6ecff5c37b589ad9ad31694f2499a27c6
jayshah41 <jay.1.shah@kcl.ac.uk>
2025-01-18 14:36:57 +0000

    Replace ContactForm's form with ContactCard component for improved structure

--- portfolio/src/Components/ContactForm.jsx
+++ portfolio/src/Components/ContactForm.jsx
@@ -1,26 +1,11 @@
 import React from 'react'
+import ContactCard from './ContactCard'
 
 const ContactForm = () => {
   return (
     <section className="section" id="ContactForm">
         <h2>Contact Me</h2>
-        <form>
-            <div>
-                <p>
-                    <label htmlFor="name">Name: </label>
-                    <input type="text" id="name" name="name" required />
-                </p>
-                <p>
-                    <label htmlFor="email">Email: </label>
-                    <input type="email" id="email" name="email" required />
-                </p>
-                <p>
-                    <label htmlFor="message">Message: </label>
-                    <textarea id="message" name="message" required></textarea>
-                </p>
-                <button type="submit">Submit</button>
-            </div>
-        </form>
+        <ContactCard />
     </section>
   )
 }
