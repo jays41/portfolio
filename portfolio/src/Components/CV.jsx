7ac497f0f89a04b094d88c8b827c58fbe7d0f284
jayshah41 <jay.1.shah@kcl.ac.uk>
2025-01-22 00:35:13 +0000

    Update CV component to download specific CV file and provide browser link

--- portfolio/src/Components/CV.jsx
+++ portfolio/src/Components/CV.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import download from "downloadjs";
+import Jay_Shah_CV from '../Jay_Shah_CV.pdf';
 
 const CV = () => {
 
     const handleDownload = () => {
-        const url = 'src/CV.pdf';
-        const fileName = 'CV';
+        const url = 'src/Jay_Shah_CV.pdf';
+        const fileName = 'Jay_Shah_CV';
         fetch(url)
         .then((response) => response.blob())
         .then((blob) => download(blob, fileName))
@@ -16,6 +17,9 @@ const CV = () => {
         <section className="section" id="CV">
             <h2>My CV</h2>
             <button onClick={handleDownload}>Download File</button>
+            <p>Alternatively, open in the browser
+            <a href={Jay_Shah_CV} target="_blank" rel="noreferrer"> here</a>
+            </p>
         </section>
     )
 }
