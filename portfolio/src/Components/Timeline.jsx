83c094da6e05f44d0eea1b23d362c5f9a89fdc31
jayshah41 <jay.1.shah@kcl.ac.uk>
2025-01-24 01:07:10 +0000

    Add Timeline component to display education and experience entries

---
+++ portfolio/src/Components/Timeline.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Timeline.css';
+import TimelineLeftItem from './TimelineLeftItem';
+import TimelineRightItem from './TimelineRightItem';
+import info from '../Info.json';
+
+const Timeline = () => {
+
+    const allItems = [
+        ...info.education.map((e) => ({...e, type: "education"})),
+        ...info.experience.map((e) => ({...e, type: "experience"}))
+    ];
+
+    allItems.sort((a, b) => new Date(a.startDate) - new Date(b.startDate)); // need to fix
+
+    const elements = (
+        <>
+            {allItems.map((data) =>
+                data.type === "education" ?
+                    (<TimelineLeftItem name={data.name} duration={data.points} points={data.points} startDate={data.startDate} />)
+                    :
+                    (<TimelineRightItem name={data.name} title={data.title} location={data.location} date={data.date} points={data.points} startDate={data.startDate} />)
+            )}
+        </>
+    );
+
+
+  return (
+    <section className="section" id="timeline">
+        <div className="timeline-container">
+            {elements}
+        </div>
+    </section>
+  )
+}
+
+export default Timeline
