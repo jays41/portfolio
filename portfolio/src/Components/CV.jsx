74639eaf0a35f4d247ffe6a9c5b97b212a105782
jayshah41 <jay.1.shah@kcl.ac.uk>
2025-01-28 21:42:30 +0000

    Add custom download button with icon and styles to CV component

--- portfolio/src/Components/CV.jsx
+++ portfolio/src/Components/CV.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import download from "downloadjs";
 import Jay_Shah_CV from '../Jay_Shah_CV.pdf';
+import './CV.css';
 
 const CV = () => {
 
@@ -16,10 +17,21 @@ const CV = () => {
     return (
         <section className="section" id="CV">
             <h2>My CV</h2>
-            <button className="btn" onClick={handleDownload}>Download File</button>
+            <button onClick={handleDownload}>Download File</button>
             <p>Alternatively, open in the browser
             <a href={Jay_Shah_CV} target="_blank" rel="noreferrer"> here</a>
             </p>
+
+            <span class="cv-button" type="button">
+            <span class="cv-button__text">Download</span>
+            <span class="cv-button__icon">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 35" class="svg">
+                <path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path>
+                <path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path>
+                <path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path>
+            </svg>
+            </span>
+            </span>
         </section>
     )
 }
